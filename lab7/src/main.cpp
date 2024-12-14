cc6b513eb811db4725f227bfdcce2448c864ce52
Keniden <nik.glumov.94@mail.ru>
2024-12-15 02:52:57 +0300

    'lab7'

--- lab7/src/main.cpp
+++ lab7/src/main.cpp
@@ -1,38 +1,96 @@
-#include <iostream>
-#include <memory>
+#include "World.h"
+#include "ObserverFile.h"
+#include "ObserverConsole.h"
+#include "Visitor.h"
+#include <random>
+#include <atomic>
 #include <thread>
-#include <chrono>
-#include "npc.hpp"
-#include "observer.hpp"
-#include "fight_manager.hpp"
+#include <mutex>
+#include <shared_mutex>
+#define WORLDSIZE 500
+
+
+std::atomic<bool> Running { true };
+std::shared_mutex npc_mutex;
 
 int main() {
-    auto consoleObs = std::make_shared<ConsoleObserver>();
-    auto fileObs = std::make_shared<FileObserver>("log.txt");
-    location.attach(consoleObs);
-    location.attach(fileObs);
+    World world(WORLDSIZE/10);
+    Visitor visitor;
+    ObserverConsole consoleObserver;
+    ObserverFile fileObserver("log.txt");
 
-    initNPCs();
+    visitor.AddObserver(&consoleObserver);
+    visitor.AddObserver(&fileObserver);
 
-    std::thread mover(moveNPCsThread);
-    std::thread fighter(battleThread);
+    double x, y, damage, health;
 
-    auto start = std::chrono::steady_clock::now();
+    const char* types[] = { "Bear", "Elf", "Robber" };
+    for (int i = 0; i < 50; ++i) {
+        damage = (rand() % 50) + 25;
+        health = (rand() % 50) + 50;
+        x = (rand() % WORLDSIZE);
+        y = (rand() % WORLDSIZE);
+        x /= 10;
+        y /= 10;
 
-    while(true) {
-        location.printMap();
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        auto now = std::chrono::steady_clock::now();
-        auto dur = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
-        if(dur >= 30) {
-            stopGame = true;
-            break;
-        }
+        std::string type = types[i % 3];
+        world.AddNPC(type, type + "_" + std::to_string(i), x, y, damage, health);
     }
-    mover.join();
-    fighter.join();
 
-    location.listSurvivors();
+    //world.Deserialize("dungeon.txt");
+    std::cout << "Fighters in dungeon:\n";
+    std::cout << "------------------------------------------------------------------" << std::endl;
+    {
+        std::shared_lock lock(npc_mutex);
+        world.AllNPC();
+    }
+    std::cout << "------------------------------------------------------------------" << std::endl;
 
+    std::thread movement_thread([&]() {
+        while (Running) {
+            {
+                std::unique_lock lock(npc_mutex);
+                world.MoveNPC();
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+
+    std::thread battle_thread([&]() {
+        while (Running) {
+            {
+                std::unique_lock lock(npc_mutex);
+                world.Engage(visitor);
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+
+    std::thread display_thread([&]() {
+        while (Running) {
+            {
+
+                std::unique_lock lock(npc_mutex);
+                world.PrintMap();
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+        }
+        });
+
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+    Running = false;
+
+    movement_thread.join();
+    battle_thread.join();
+    display_thread.join();
+
+    std::cout << "\nRemaining Fighters:\n";
+    std::cout << "------------------------------------------------------------------" << std::endl;
+    {
+        std::shared_lock lock(npc_mutex);
+        world.PrintMap();
+        world.AllNPC();
+    }
+    std::cout << "------------------------------------------------------------------" << std::endl;
     return 0;
 }
